1a3f5831b17e930fa28e0636cdbd8f965f4e887a
ananthu-16 <raamanantha.16@gmail.com>
2020-04-05 15:14:20 +0530

    header

--- headers.h
+++ headers.h
@@ -6,9 +6,13 @@
 #include<termios.h>
 #include<stdio.h>
 #include<vector>
+#include<stdlib.h>
 using namespace std;
 extern vector< string > dir_ent;
 extern char *root;
-extern int rsize,csize;
+extern int curr_x,csize;
 extern int tot_file;
 void ExploreDirectory(const char *dir_name);
+void FileOptions();
+void clrscr();
+void gotoxy(int,int);
