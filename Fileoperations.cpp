766f2e2dbff4c664e29990f8c5c422a87b6d1021
ananthu-16 <raamanantha.16@gmail.com>
2020-04-05 15:13:25 +0530

    implemented up-down functionalities

--- Fileoperations.cpp
+++ Fileoperations.cpp
@@ -1,36 +1,69 @@
 #include"headers.h"
-int xcor=1,ycor=70;
-#define esc 27
-#define pos() printf("%c[%d;%dH", esc, xcor, ycor)
-#define posx(x,y) printf("%c[%d;%dH", esc, x, y)
+
+//int tot_file;
+int curr_x;
+
+void clrscr()
+{
+	printf("\e[1;1H\e[2J");
+}
+void gotoxy (int x, int y)
+{
+    printf("%c[%d;%df",0x1B,x,y);      
+}
+
 void FileOptions()
 {
-    char ch;
-    xcor=1;
-    ycor=70;
-    pos();
-    int lastline;
+  
+	vector<string>::iterator ptr; 
     struct termios old, news;
+	curr_x=0;
 	tcgetattr(fileno(stdin), &old);
 	news = old;
 	news.c_lflag &= ~ICANON;
 	news.c_lflag &= ~ECHO;
 
-	if (tcsetattr(fileno(stdin),TCSAFLUSH,&new) != 0)
+	if (tcsetattr(fileno(stdin),TCSAFLUSH,&news) != 0)
 	{
 		fprintf(stderr, "Could not set attributes\n");
 	}
-    else
-    {
-        while(1)
-        {
-            lastline=rowsize+1;
-            posx(lastline,1);
-            cout<<"Welcome to File operations mode\n";
-            pos();
-            ch=cin.get()
-            
-        }
-    }
-    
+	else
+	{
+		char  ch;
+		gotoxy(0,0);
+		while(1)
+		{
+			ch=cin.get();
+			if(ch=='w')		//up arrow
+			{
+				curr_x--;
+				if(curr_x<0)
+				{
+					curr_x=0;
+				}
+				gotoxy(curr_x,0);
+				// for (ptr = dir_ent.begin(); ptr < dir_ent.end(); ptr++)
+				// {
+				// 	cout<<*ptr<<"\n";
+				// }
+			}
+			else if(ch=='d') //down arrow
+			{
+				curr_x++;
+				if(curr_x>tot_file)
+				{
+					curr_x=tot_file;
+				}
+				gotoxy(curr_x,0);
+			}
+			else if(ch=='q')	//quit filemanager
+			{
+				clrscr();
+				break;
+			}
+			
+		}
+	}
+	clrscr();
+	tcsetattr(fileno(stdin), TCSANOW, &old);
 }
