3eea6098d22e588b8210274535c9d3481807d4f3
ananthu-16 <raamanantha.16@gmail.com>
2020-04-07 00:30:00 +0530

    commiting file operations

--- headers.h
+++ headers.h
@@ -9,16 +9,19 @@
 #include<stdio.h>
 #include<vector>
 #include<stdlib.h>
+using namespace std;
+
 #define RESET   "\033[0m"
 #define RED "\033[31m"
 #define GREEN "\033[32m"
-using namespace std;
-extern vector< string > dir_ent;
+
+
+extern vector <string> dir_ent;
 extern char *root;
 extern int curr_x,csize;
 extern int tot_file;
+
 void ExploreDirectory(const char *dir_name);
 void FileOptions();
 void clrscr();
 void gotoxy(int,int);
-//int is_regular_file(const char*);
