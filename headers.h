f6be45f3800d5a2334dc622054e124a5233da7ec
ananthu-16 <raamanantha.16@gmail.com>
2020-04-06 17:41:59 +0530

    commiting files

--- headers.h
+++ headers.h
@@ -3,10 +3,15 @@
 #include<string.h>
 #include<dirent.h>
 #include<sys/stat.h>
+#include<sys/types.h>
+#include<unistd.h>
 #include<termios.h>
 #include<stdio.h>
 #include<vector>
 #include<stdlib.h>
+#define RESET   "\033[0m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
 using namespace std;
 extern vector< string > dir_ent;
 extern char *root;
@@ -16,3 +21,4 @@ void ExploreDirectory(const char *dir_name);
 void FileOptions();
 void clrscr();
 void gotoxy(int,int);
+//int is_regular_file(const char*);
