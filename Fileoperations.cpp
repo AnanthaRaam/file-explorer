ea84e55c84053c8fd2377552364391be67158e14
ananthu-16 <raamanantha.16@gmail.com>
2020-04-07 16:50:18 +0530

    commiting files

--- Fileoperations.cpp
+++ Fileoperations.cpp
@@ -4,14 +4,7 @@ using namespace std;
 int curr_x=1;
 vector<string> dir_ent;
 
-void clrscr()
-{
-	printf("\e[1;1H\e[2J");
-}
-void gotoxy (int x, int y)
-{
-    printf("%c[%d;%df",0x1B,x,y);      
-}
+
 
 void FileOptions()
 { 
@@ -53,12 +46,49 @@ void FileOptions()
 			}
 			else if(int(ch)==10)	//enter key
 			{
-				string temp=dir_ent[curr_x-1];;
-				ExploreDirectory(temp.c_str());
+				struct stat stats;
+				string t = dir_ent[curr_x-1];
+				stat(t.c_str(),&stats);
+        		if(S_ISDIR(stats.st_mode))
+				{
+					ExploreDirectory(t.c_str());
+				}
+				else
+				{
+					if (stat(t.c_str(), &stats) == 0)
+    				{
+        				printFileProperties(stats);
+    				}
+    				else
+    				{
+        				printf("Unable to get file properties.\n");
+        				printf("Please check whether '%s' file exists.\n",t.c_str());
+    				}
+					cout<<"\npress any key to continue...";
+					getchar();
+					clrscr();
+				}
+				curr_x=1;
+				gotoxy(curr_x,1);
+				struct stat info;
+				for(int i=0;i<tot_file;i++)
+				{
+					string t = dir_ent[i];
+					stat(t.c_str(),&info);
+        			if(S_ISDIR(info.st_mode))
+					{
+           				cout<<GREEN<<t<<RESET<<endl;
+					}
+					else
+					{
+						cout<<RED<<t<<RESET<<endl;
+					}
+		
+				}
 				curr_x=1;
 				gotoxy(curr_x,1);
 			}
-			else if(ch==0x7f)
+			else if(ch==0x7f) 	//backspace entered
 			{
 				clrscr();
 				//cout<<"backspace entered\n";
@@ -77,6 +107,36 @@ void FileOptions()
 				}
 				curr_x=1;
 				gotoxy(curr_x,1);
+			}
+			else if(ch=='>')
+			{
+				string oldname=dir_ent[curr_x-1];
+				gotoxy(tot_file+1,1);
+				char f;
+				cout<<"do you want to rename this file[y/n]?\n";
+				cin>>f;
+				if(f=='y')
+				{
+					
+					string newname;
+					cout<<"\noldname is:"<<oldname;
+					cout<<"\nenter new name of the file (with .extension): ";
+					cin>>newname;
+					newname=oldname.substr(0,oldname.find_last_of("//"))+"/"+newname;
+					getchar();
+					rename(oldname.c_str(),newname.c_str());
+					ExploreDirectory(newname.substr(0,newname.find_last_of("//")).c_str());
+					curr_x=1;
+					gotoxy(curr_x,1);
+				}
+				else
+				{
+					ExploreDirectory(oldname.substr(0,oldname.find_last_of("//")).c_str());
+					curr_x=1;
+					gotoxy(curr_x,1);
+				}
+				
+
 			}
 			else if(ch=='q')	//quit filemanager
 			{
