8098236441616a800d6fe9190122693d4be30764
ananthu-16 <raamanantha.16@gmail.com>
2020-04-07 16:51:15 +0530

    commiting files

--- headers.h
+++ headers.h
@@ -14,7 +14,7 @@ using namespace std;
 #define RESET   "\033[0m"
 #define RED "\033[31m"
 #define GREEN "\033[32m"
-
+#define CYAN  "\x1B[36m"
 
 extern vector <string> dir_ent;
 extern char *root;
@@ -23,5 +23,6 @@ extern int tot_file;
 
 void ExploreDirectory(const char *dir_name);
 void FileOptions();
+void printFileProperties(struct stat stats);
 void clrscr();
 void gotoxy(int,int);
