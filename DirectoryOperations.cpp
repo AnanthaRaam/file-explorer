d11bc180f6ba9dfd6acb223f1de5025378e226ea
ananthu-16 <raamanantha.16@gmail.com>
2020-04-07 16:48:50 +0530

    file rename option added

--- DirectoryOperations.cpp
+++ DirectoryOperations.cpp
@@ -38,6 +38,26 @@ int directorylist(const char *dir_name=NULL)
 	return count;
 }
 
+void printFileProperties(struct stat stats)
+{
+	clrscr();
+    struct tm dt;
+    cout<<CYAN<<"File access: "<<RESET;
+    if (stats.st_mode & R_OK)
+        cout<<CYAN<<"read "<<RESET;
+    if (stats.st_mode & W_OK)
+        cout<<CYAN<<"write "<<RESET;
+    if (stats.st_mode & X_OK)
+        cout<<CYAN"execute"<<RESET;
+    cout<<CYAN<<"\nFile size: "<<stats.st_size<<"kb"<<RESET;
+    dt = *(gmtime(&stats.st_ctime));
+    cout<<CYAN<<"\nCreated on: "<<dt.tm_mday<<"-"<<dt.tm_mon<<"-"<<dt.tm_year + 1900<< 
+                                             " "<<dt.tm_hour<<":"<<dt.tm_min<<":"<<dt.tm_sec<<RESET;
+    dt = *(gmtime(&stats.st_mtime));
+    cout<<CYAN<<"\nModified on: "<<dt.tm_mday<<"-"<<dt.tm_mon<<"-"<<dt.tm_year + 1900<<
+                                              " "<<dt.tm_hour<<":"<<dt.tm_min<<":"<<dt.tm_sec<<RESET;
+}
+
 void ExploreDirectory(const char *dir_name=NULL)
 {
     clrscr();
