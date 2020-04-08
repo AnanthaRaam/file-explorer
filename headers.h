454716f10bdb388267be23cbc04b609feb644434
ananthu-16 <raamanantha.16@gmail.com>
2020-04-08 19:22:25 +0530

    commiting header file

--- headers.h
+++ headers.h
@@ -1,3 +1,4 @@
+/* required headers */
 #include<fstream>
 #include<iostream>
 #include<string>
@@ -13,18 +14,57 @@
 #include<stdlib.h>
 using namespace std;
 
+/* setting color properties */
 #define RESET   "\033[0m"
 #define RED "\033[31m"
 #define GREEN "\033[32m"
 #define CYAN  "\x1B[36m"
 
+/*  GLOBAL VARIABLES */
+
+/* to store contents in a directory */
 extern vector <string> dir_ent;
-extern char *root;
-extern int curr_x,csize;
+extern string root;
+
+/* current position of cursor */ 
+extern int curr_x;
+
+/* total files in a directory */
 extern int tot_file;
 
+
+
+/* GLOBAL METHODS */
+
+/* printing files from a directory */
 void ExploreDirectory(const char *dir_name);
-void FileOptions();
-void printFileProperties(struct stat stats);
+
+/* getting user input to do specific task */
+void FileExplorerOptions();
+
+/* printing file properties */
+void PrintFileProperties(struct stat stats);
+
+/* exploring file upwards */
+void ExploreFileUpwards();
+void ExploreFileDownwards();
+
+/* navigating the file contents*/
+void NavigateContentUpwards();
+void NavigateContentDownwards();
+
+/* rename the file or folder*/
+void RenameFileOrFolder();
+
+/* delete file or folder */
+void DeleteFileOrFolder();
+
+/* create folder */
+void CreateFolder();
+
+/* create file */
+void CreateFile();
+
+/* extrautility functions */
 void clrscr();
 void gotoxy(int,int);
